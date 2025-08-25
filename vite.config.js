f058f3de73723923f970a2e4af92151a131ae912
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-08-26 00:22:11 +0100

    add dev server

--- vite.config.js
+++ vite.config.js
@@ -36,14 +36,14 @@ export default defineConfig({
             }
         }
     },
-    // server: {
-    //     host: true,
-    //     port: 5173,
-    //     origin: 'http://83.147.36.29:5173',
-    //     cors: {
-    //         origin: 'http://83.147.36.29',
-    //         credentials: true,
-    //     },
-    // },
+    server: {
+        host: true,
+        port: 5173,
+        origin: 'http://83.147.36.29:5173',
+        cors: {
+            origin: 'http://83.147.36.29',
+            credentials: true,
+        },
+    },
 
 });
