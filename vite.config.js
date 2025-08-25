861ca29fdea76702ffeff7564e0aff1b620cc53d
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-08-25 19:26:56 +0100

    debug

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            ['resources/css/app.css', 'resources/js/app.tsx'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
