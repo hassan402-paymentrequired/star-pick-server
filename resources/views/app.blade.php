3c21d8676e636c13f575bcf53c5d15f760d020a4
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-09-05 19:34:04 +0100

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,10 +28,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @routes
-        @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-        @inertiaHead
+      @routes
+@vite('resources/js/app.tsx')
+@inertiaHead
+
     </head>
     <body class="font-sans antialiased">
         @inertia
