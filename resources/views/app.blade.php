d79c5208c6813335340517db4e87c98b18221c9c
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-09-06 14:59:57 +0100

    set dynamically

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,17 +9,17 @@
         content="VibezSync is a social media scheduling and content management platform that allows creators and businesses to plan, schedule, and publish posts across multiple platforms, including TikTok, Instagram, and YouTube.">
 
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://starpick.com.ng">
+    <meta property="og:url" content="https://starpick.com.ng  ">
     <meta property="og:title" content="Content Title">
-    <meta property="og:image" content="https://starpick.com.ng/image.jpg">
+    <meta property="og:image" content="https://starpick.com.ng/image.jpg  ">
     <meta property="og:description" content="Description Here" />
     <meta property="og:site_name" content="Site Name">
     <meta property="og:locale" content="en_US">
     <!-- Next tags are optional but recommended -->
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="630">
-    <link rel="dns-prefetch" href="https://starpick.com.ng/" />
-    <link rel="preconnect" href="https://starpick.com.ng/" />
+    <link rel="dns-prefetch" href="https://starpick.com.ng/  " />
+    <link rel="preconnect" href="https://starpick.com.ng/  " />
 
     <title inertia>{{ config('app.name', 'Starpick') }}</title>
 
@@ -27,10 +27,13 @@
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="preconnect" href="https://fonts.bunny.net  ">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     @routes
-    @viteReactRefresh
+    {{-- ✅ REMOVED: @viteReactRefresh --}}
+    @if (app()->environment('local'))
+        @viteReactRefresh
+    @endif
     @vite('resources/js/app.tsx')
     @inertiaHead
 </head>
