9feeca9a8ee5325ffa972989ddaf96d08bf7c92c
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-09-06 14:38:40 +0100

    new update

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,41 +1,30 @@
-import "../css/app.css";
 import "./bootstrap.js";
+import "../css/app.css";
 
+import { Toaster } from "./components/ui/sonner";
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 
+const appName = import.meta.env.VITE_APP_NAME || "Starpick";
+
 createInertiaApp({
+    title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.tsx", { eager: true });
         return pages[`./Pages/${name}.tsx`];
     },
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />);
+        const root = createRoot(el);
+
+        root.render(
+            <>
+                <Toaster />
+                <App {...props} />
+            </>
+        );
+    },
+    progress: {
+        color: "#4B5563",
+        showSpinner: true,
     },
 });
-
-// import { createInertiaApp } from '@inertiajs/react';
-// import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-// import { createRoot } from 'react-dom/client';
-// import { Toaster } from './components/ui/sonner';
-
-// const appName = import.meta.env.VITE_APP_NAME || 'Starpick';
-
-// createInertiaApp({
-//     title: (title) => (title ? `${title} - ${appName}` : appName),
-//     resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
-//     setup({ el, App, props }) {
-//         const root = createRoot(el);
-
-//         root.render(
-//             <>
-//                 <Toaster />
-//                 <App {...props} />
-//             </>,
-//         );
-//     },
-//     progress: {
-//         color: '#4B5563',
-//         showSpinner: true
-//     },
-// });
