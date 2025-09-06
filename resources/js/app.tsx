1dc1d293aed8270db75308d8048aac75acedac24
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-09-06 13:39:19 +0100

    update

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,4 +1,5 @@
 import "../css/app.css";
+import "../css/boostrap.js";
 
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,7 +30,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     @routes
-    @viteReactRefresh
+    {{-- @viteReactRefresh --}}
     @vite('resources/js/app.tsx')
     @inertiaHead
 </head>
