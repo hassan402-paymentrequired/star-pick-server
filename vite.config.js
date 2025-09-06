b027c5fb24264cb3363389ab30682959f703ac53
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-09-06 14:46:15 +0100

    git push

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,15 @@ export default defineConfig({
             '@': '/resources/js',
         },
     },
+    server: {
+        host: true,
+        port: 5173,
+        origin: 'https://starpick.com.ng:5173',
+        cors: {
+            origin: 'https://starpick.com.ng',
+            credentials: true,
+        },
+    },
 
 
 });
